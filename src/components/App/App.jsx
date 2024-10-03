a89a71353b86b9cfe7e595fe462ae9d040f95d88
Matthew Black <matthewallenblack@gmail.com>
2024-10-03 10:12:16 -0500

    implement header/main/footer structure

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -21,57 +21,64 @@ function App() {
   }, [fetchUser]);
 
   return (
-    <div>
-      <h1>Prime Solo Project</h1>
-      <Nav />
-      <Routes>
-        <Route 
-          exact path="/"
-          element={
-            user.id ? (
-              <HomePage /> // Render HomePage for authenticated user.
-            ) : (
-              <Navigate to="/login" replace /> // Redirect unauthenticated user.
-            )
-          }
-        />
-        <Route 
-          exact path="/login"
-          element={
-            user.id ? (
-              <Navigate to="/" replace /> // Redirect authenticated user.
-            ) : (
-              <LoginPage /> // Render LoginPage for unauthenticated user.
-            )
-          }
-        />
-        <Route 
-          exact path="/registration"
-          element={
-            user.id ? (
-              <Navigate to="/" replace /> // Redirect authenticated user.
-            ) : (
-              <RegisterPage /> // Render RegisterPage for unauthenticated user.
-            )
-          }
-        />
-        <Route 
-          exact path="/about"
-          element={
-            <>
-              <h2>About Page</h2>
-              <p>🌵🦊🌈🌍</p>
-            </>
-          }
-        />
-        <Route
-          path="*"
-          element={
-            <h2>404 Page</h2>
-          } 
-        />
-      </Routes>
-    </div>
+    <>
+      <header>
+        <h1>Prime Solo Project</h1>
+        <Nav />
+      </header>
+      <main>
+        <Routes>
+          <Route 
+            exact path="/"
+            element={
+              user.id ? (
+                <HomePage /> // Render HomePage for authenticated user.
+              ) : (
+                <Navigate to="/login" replace /> // Redirect unauthenticated user.
+              )
+            }
+          />
+          <Route 
+            exact path="/login"
+            element={
+              user.id ? (
+                <Navigate to="/" replace /> // Redirect authenticated user.
+              ) : (
+                <LoginPage /> // Render LoginPage for unauthenticated user.
+              )
+            }
+          />
+          <Route 
+            exact path="/registration"
+            element={
+              user.id ? (
+                <Navigate to="/" replace /> // Redirect authenticated user.
+              ) : (
+                <RegisterPage /> // Render RegisterPage for unauthenticated user.
+              )
+            }
+          />
+          <Route 
+            exact path="/about"
+            element={
+              <>
+                <h2>About Page</h2>
+                <p>🌵🦊🌈🌍</p>
+              </>
+            }
+          />
+          <Route
+            path="*"
+            element={
+              <h2>404 Page</h2>
+            } 
+          />
+        </Routes>
+      </main>
+      <footer>
+        <p>Copyright © {new Date().getFullYear()}</p>
+      </footer>
+    </>
   );
 }
 
