00e30eae5b89ef7b829610120c54232b7656d944
windyashes <sethkemmet@gmail.com>
2024-11-12 14:29:24 -0600

    donation creation and updation

--- server/routes/action.router.js
+++ server/routes/action.router.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const pool = require('../modules/pool');
+const { rejectUnauthenticated } = require('../modules/authentication-middleware');
 
 const router = express.Router();
+
+/** 
+ * need donation post, get, put, delete
+ * 
+ * 
+ */
 
 /**
  * @swagger
@@ -65,7 +72,7 @@ const router = express.Router();
  *                  type: string
  *                  description: Error message indicating an internal server error.
  */
-router.get('/:userId', (req, res) => {
+router.get('/:userId',rejectUnauthenticated, (req, res) => {
     const queryText = `
     select 
 	(select json_agg(json_build_object(
@@ -110,6 +117,142 @@ router.get('/:userId', (req, res) => {
     })
 })
 
+/**
+ * @swagger
+ * openapi: 3.0.0
+ * info:
+ *   title: Donations API
+ *   version: 1.0.0
+ *   description: API for managing donations.
+ * paths:
+ *   /api/actions/donation:
+ *     post:
+ *       tags:
+ *         - donations
+ *       summary: Create a new donation
+ *       description: Inserts a new donation record into the database.
+ *       requestBody:
+ *         required: true
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 user_id:
+ *                   type: integer
+ *                   description: The ID of the user making the donation.
+ *                 amount:
+ *                   type: number
+ *                   format: float
+ *                   description: The amount of the donation.
+ *               required:
+ *                 - user_id
+ *                 - amount
+ *       responses:
+ *         '200':
+ *           description: Donation successfully created.
+ *         '400':
+ *           description: Bad request. Invalid input.
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 type: object
+ *                 properties:
+ *                   error:
+ *                     type: string
+ *                     description: Error message detailing the invalid input.
+ *         '500':
+ *           description: Internal server error.
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 type: object
+ *                 properties:
+ *                   error:
+ *                     type: string
+ *                     description: Error message indicating an internal server error.
+ */
+router.post('/donation', rejectUnauthenticated, (req, res) => {
+    const queryText = `
+    insert into donation (user_id, amount)
+    values ($1, $2);
+    `
+    pool.query(queryText, [req.body.user_id, req.body.amount]).then(response => {
+        res.sendStatus(200);
+    }).catch(err => {
+        console.error('Error creating donation', err);
+        res.send(500);
+    })
+})
+
+/**
+ * @swagger
+ * openapi: 3.0.0
+ * info:
+ *   title: Donations API
+ *   version: 1.0.0
+ *   description: API for managing donations.
+ * paths:
+ *   /api/actions/donation:
+ *     put:
+ *       tags:
+ *         - donations
+ *       summary: update the amount of an existing donation
+ *       description: Updates the amount of the donation mand sets a new updated at time.
+ *       requestBody:
+ *         required: true
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 amount:
+ *                   type: number
+ *                   format: float
+ *                   description: The amount of the donation.
+ *                 id:
+ *                   type: integer
+ *                   description: The ID of the donation.
+ *               required:
+ *                 - amount
+ *                 - id
+ *       responses:
+ *         '200':
+ *           description: Donation successfully updated.
+ *         '400':
+ *           description: Bad request. Invalid input.
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 type: object
+ *                 properties:
+ *                   error:
+ *                     type: string
+ *                     description: Error message detailing the invalid input.
+ *         '500':
+ *           description: Internal server error.
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 type: object
+ *                 properties:
+ *                   error:
+ *                     type: string
+ *                     description: Error message indicating an internal server error.
+ */
+router.put('/donation', rejectUnauthenticated, (req, res) => {
+    const queryText = `
+    update donation 
+    set amount = $1, updated_at = (now() AT TIME ZONE 'utc'::text)
+    where id = $2;
+    `
+    pool.query(queryText, [req.body.amount, req.body.id]).then(response => {
+        res.sendStatus(200);
+    }).catch(err => {
+        console.error('Error updating donation', err);
+        res.send(500);
+    })
+})
 
 
 module.exports = router;
