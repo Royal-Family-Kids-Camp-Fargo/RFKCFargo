f3bc6e9a14ee9f00528681f1f6f3b4ff1d951ef1
JennyLagervall <156864102+JennyLagervall@users.noreply.github.com>
2024-11-18 11:24:50 -0600

    pipeline dropdown working

--- src/components/Pipeline/Pipeline.jsx
+++ src/components/Pipeline/Pipeline.jsx
@@ -4,11 +4,23 @@ import useStore from '../../zustand/store';
 export default function Pipeline() {
   const pipelines = useStore((state) => state.pipelines);
   const fetchPipeline = useStore((state) => state.fetchPipeline);
-  console.log('store data', pipelines);
+  const [pipeline, setPipeline] = useState('');
 
   useEffect(() => {
     fetchPipeline();
   }, [fetchPipeline]);
+
+  const ChoosePipeline = (event) => {
+    event.preventDefault();
+
+    //  dispatch({
+    //    type: 'NEW_LOG',
+    //    payload: {
+    //      details: details,
+    //    },
+    //  });
+    //  setPipeline('');
+  };
 
   //   const fetchPipeline = async () => {
   //     const response = await axios.get('/api/pipeline');
@@ -17,8 +29,20 @@ export default function Pipeline() {
   //   };
 
   return (
-    <div>
-      <h1>Pipeline page!</h1>;<p>{pipelines?.length > 0 && JSON.stringify(pipelines)}</p>
-    </div>
+    <>
+      <div>
+        <h1>Pipeline page!</h1>;<p>{pipelines?.length > 0 && JSON.stringify(pipelines)}</p>
+      </div>
+      <div>
+        <label for='pipelines'>Choose pipeline</label>
+        <select value={pipeline} onChange={(event) => setPipeline(event.target.value)}>
+          {pipelines.map((pipeline) => (
+            <option key={pipeline.id} value={pipeline.id}>
+              {pipeline.name}
+            </option>
+          ))}
+        </select>
+      </div>
+    </>
   );
 }
--- src/zustand/slices/pipeline.slice.js
+++ src/zustand/slices/pipeline.slice.js
@@ -2,8 +2,27 @@ import axios from 'axios';
 
 const createPipelineSlice = (set, get) => ({
   pipelines: [],
+  selectedPipeline: {},
+  setSelectedPipeline: (pipeline) =>
+    set(() => ({
+      selectedPipeline: pipeline,
+    })),
   fetchPipeline: async () => {
     //  Retrieves the pipelines data from the /api/pipeline endpoint.
+    try {
+      const { data } = await axios.get('/api/pipeline');
+      console.log('data from store', data);
+      set({ pipelines: data });
+    } catch (err) {
+      console.log('fetchPipeline error:', err);
+      set({ pipelines: [] });
+    }
+  },
+
+
+  fetchPipelineById: async () => {
+     // Retrieves the selected pipeline from /api/pipeline/:pipelineId endpoint.
+     // going to need a pipeline id as a payload from Pipeline component
     try {
       const { data } = await axios.get('/api/pipeline');
       console.log('data from store', data);
