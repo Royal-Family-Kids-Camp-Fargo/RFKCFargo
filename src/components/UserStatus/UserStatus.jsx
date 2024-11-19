e247594acb87a4c35ef3ecb9a018f11aa43d2d71
JennyLagervall <156864102+JennyLagervall@users.noreply.github.com>
2024-11-19 11:13:57 -0600

    Can click person on pipeline

--- src/components/UserStatus/UserStatus.jsx
+++ src/components/UserStatus/UserStatus.jsx
@@ -15,20 +15,29 @@ export default function UserStatus({ person }) {
     // need pipeline_status_id AND the user_id
     // axios call to endpoint PUT: /api/pipeline/user_status
     // build up the object: {pipeline_status_id: pipelineStatus, user_id: p.id}
-    axios
-      .put('/api/pipeline/user_status', { pipeline_status_id: pipelineStatus, user_id: p.id })
-      .then((response) => {
-        console.log('moved swim lanes OK');
-        // look to refresh the data...
-        console.log('mySelectedPipeline', mySelectedPipeline);
-        fetchPipelineById(mySelectedPipeline.pipeline_id);
-      })
-      .catch((error) => console.log('Error in moving swim lanes', error));
+
+    if (pipelineStatus) {
+      axios
+        .put('/api/pipeline/user_status', { pipeline_status_id: pipelineStatus, user_id: p.id })
+        .then((response) => {
+          console.log('moved swim lanes OK');
+          // look to refresh the data...
+          console.log('mySelectedPipeline', mySelectedPipeline);
+          fetchPipelineById(mySelectedPipeline.pipeline_id);
+        })
+        .catch((error) => console.log('Error in moving swim lanes', error));
+    } else {
+      alert('Please select a status');
+    }
+  };
+  const goToProfile = (personId) => {
+    // eventually navigate to this person profile page
+    console.log('Moving to profile!', personId);
   };
 
   if (person.id) {
     return (
-      <div key={person.id}>
+      <div key={person.id} style={{ border: '1px solid black' }} onClick={() => goToProfile(person.id)}>
         {person.user_firstName}
 
         <div>
@@ -43,7 +52,7 @@ export default function UserStatus({ person }) {
           </select>
         </div>
 
-        <button onClick={() => moveLane(person)}>Next</button>
+        <button onClick={() => moveLane(person)}>Move</button>
       </div>
     );
   } else return <div>No One in Status</div>;
