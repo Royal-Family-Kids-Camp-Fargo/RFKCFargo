a512ceb0ef7550f03934a653fb4e114874f4ab5c
Blaine Booher <bgbooher@gmail.com>
2024-11-22 23:51:50 -0600

    home page cleanup

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,36 +1,50 @@
 import useStore from '../../zustand/store'
 import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
-
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
 
 function HomePage() {
   const user = useStore((state) => state.user);
-  const logOut = useStore((state) => state.logOut);
   const allForms = useStore(store => store.allForms);
   const fetchForms = useStore(store => store.fetchForms);
 
-  // TODO: Create a button for each form type.
-  // If not logged in they are sent to register/login page
-  // When clicked, performs a POST request to create 
-  // the initial form.
-  // Grab the form ID and redirect the user to that page
   useEffect(() => {
     fetchForms();
-  }, [])
+  }, []);
+
   return (
-    <>
-      <h2>Home Page</h2>
-      <p>Your username is: {user.username}</p>
-      <p>Your ID is: {user.id}</p>
-      {allForms.map(form => (
-        <Link to={`/form/${form.id}/0`}>{form.name}</Link>
-      ))}
-      <button onClick={logOut}>
-        Log Out
-      </button>
-    </>
+    <Container>
+      <Row className="mb-4">
+        <Col>
+          <h2 className="text-center">Welcome, {user.username}!</h2>
+        </Col>
+      </Row>
+
+      <Row xs={1} md={2} lg={3} className="g-4">
+        {allForms.map(form => (
+          <Col key={form.id}>
+            <Card className="h-100 shadow-sm">
+              <Card.Body>
+                <Card.Title>{form.name}</Card.Title>
+                <Card.Text>
+                  Form ID: {form.id}
+                </Card.Text>
+                <Link 
+                  to={`/form/${form.id}/0`}
+                  className="btn btn-primary stretched-link"
+                >
+                  Start Form
+                </Link>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </Container>
   );
 }
-
 
 export default HomePage;
