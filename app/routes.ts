be9169ad0b034f2eb9508e93380c8dd8284ea526
Zac <zpaulson001@gmail.com>
2025-01-29 16:03:17 -0800

    comment out sign-up

--- app/routes.ts
+++ app/routes.ts
@@ -1,16 +1,16 @@
-import { type RouteConfig, index, route } from "@react-router/dev/routes";
+import { type RouteConfig, index, route } from '@react-router/dev/routes';
 
 export default [
-  index("routes/home.tsx"),
-  route("sign-up", "routes/sign-up.tsx"),
-  route("sign-in", "routes/sign-in.tsx"),
-  route("dashboard", "routes/dashboard/dashboard.tsx", [
+  index('routes/home.tsx'),
+  // route("sign-up", "routes/sign-up.tsx"),
+  route('sign-in', 'routes/sign-in.tsx'),
+  route('dashboard', 'routes/dashboard/dashboard.tsx', [
     route(
-      "pipelines/:pipelineId",
-      "routes/dashboard/pipelines/ViewPipeline.tsx"
+      'pipelines/:pipelineId',
+      'routes/dashboard/pipelines/ViewPipeline.tsx'
     ),
-    route("forms/:id", "routes/dashboard/forms/ViewForm.tsx"),
-    route("announcements", "routes/dashboard/announcements/announcements.tsx"),
-    route("sms-templates", "routes/dashboard/sms-templates/sms-templates.tsx"),
+    route('forms/:id', 'routes/dashboard/forms/ViewForm.tsx'),
+    route('announcements', 'routes/dashboard/announcements/announcements.tsx'),
+    route('sms-templates', 'routes/dashboard/sms-templates/sms-templates.tsx'),
   ]),
 ] satisfies RouteConfig;
