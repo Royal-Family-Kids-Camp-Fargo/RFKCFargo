cfed6f84d682e5a2b06c78147966e73f2fcd7829
Zac <zpaulson001@gmail.com>
2025-02-04 21:45:41 -0800

    fix breadcrumbs

--- app/components/TopNav.tsx
+++ app/components/TopNav.tsx
@@ -4,16 +4,39 @@ import {
   Breadcrumb,
   BreadcrumbItem,
   BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbSeparator,
 } from '~/components/ui/breadcrumb';
 import { Menu } from 'lucide-react';
 import type { User } from '~/api/objects/user';
 import { RfkCentralIcon } from './icons/RfkCentralIcon';
+import { Link, useLocation } from 'react-router';
 
 type TopNavProps = {
   setSideBarOpen: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
+type Breadcrumb = {
+  label: string;
+  path: string;
+};
+
 export function TopNav({ setSideBarOpen }: TopNavProps) {
+  const location = useLocation();
+  const breadcrumbs = location.pathname.split('/').filter(Boolean);
+  const breadcrumbItems: Breadcrumb[] = [];
+
+  for (let i = 0; i < breadcrumbs.length; i++) {
+    const breadcrumb = breadcrumbs[i];
+    const path = breadcrumbs.slice(0, i + 1).join('/');
+    breadcrumbItems.push({
+      label: breadcrumb.charAt(0).toUpperCase() + breadcrumb.slice(1),
+      path: `/${path}`,
+    });
+  }
+
+  console.log(breadcrumbItems);
+
   return (
     <div className="flex py-2 items-center px-4 gap-2">
       <Button
@@ -32,12 +55,18 @@ export function TopNav({ setSideBarOpen }: TopNavProps) {
       {/* Breadcrumbs */}
       <div className="flex-1">
         <Breadcrumb>
-          <BreadcrumbItem>
-            <BreadcrumbLink href="/">Home</BreadcrumbLink>
-          </BreadcrumbItem>
-          <BreadcrumbItem>
-            <BreadcrumbLink href="#">Current Page</BreadcrumbLink>
-          </BreadcrumbItem>
+          <BreadcrumbList>
+            {breadcrumbItems.map((breadcrumb, index) => (
+              <>
+                <BreadcrumbItem key={index}>
+                  <BreadcrumbLink asChild>
+                    <Link to={breadcrumb.path}>{breadcrumb.label}</Link>
+                  </BreadcrumbLink>
+                </BreadcrumbItem>
+                {index < breadcrumbItems.length - 1 && <BreadcrumbSeparator />}
+              </>
+            ))}
+          </BreadcrumbList>
         </Breadcrumb>
       </div>
 
